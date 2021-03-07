4e5c14b188a422e3b3bfd942d662ab30c1ca3ec1
JavierCaparo <55814804+jfcb853@users.noreply.github.com>
2021-03-07 17:30:16 -0500

    adding fakerestapi apis as collection

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -8,5 +8,5 @@ resource "azurerm_subnet" "test" {
   name                 = "${var.application_type}-${var.resource_type}-sub"
   resource_group_name  = var.resource_group
   virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes       = var.address_prefix_test
+  address_prefix       = var.address_prefix_test
 }
