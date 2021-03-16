2e0aef6167c163314a6670c5e8622b2f75e2abab
JavierCaparo <55814804+jfcb853@users.noreply.github.com>
2021-03-16 12:16:32 -0500

    terraform deleted on pipeline -execuetd manually

--- deploy_log_analytics_workspace.sh
+++ deploy_log_analytics_workspace.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+az group create --name RG-LogAnalytics --location eastus2
 az deployment group create --resource-group RG-LogAnalytics --name test-log --template-file deploy_log_analytics_workspace.json
